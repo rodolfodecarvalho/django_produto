fce4d244b435be427be774300a248a27c75c2b96
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-04-23 22:10:30 -0300

    feat: ajustes settings

--- produto/views.py
+++ produto/views.py
@@ -2,29 +2,29 @@ from django.shortcuts import redirect, render
 
 from produto.models import Produto
 
+
 def cadastrar(request):
-    if request.method == 'GET':
+    
+    if request.method == "GET":
         produtos = Produto.objects.all()
-        
+
         soma = sum(obj.preco for obj in produtos)
 
-        return render(request, 'cadastrar.html', {'produtos': produtos, 'soma':soma})
-    elif request.method == 'POST':
-        nome = request.POST.get('inputProduto')
-        preco = request.POST.get('inputPreco')
+        return render(request, "cadastrar.html", {"produtos": produtos, "soma": soma})
+    elif request.method == "POST":
+        nome = request.POST.get("inputProduto")
+        preco = request.POST.get("inputPreco")
 
-        produto = Produto(
-            nome=nome,
-            preco=preco
-        )
+        produto = Produto(nome=nome, preco=preco)
 
         produto.save()
 
-        return redirect('cadastrar')
-    
+        return redirect("cadastrar")
+
+
 def exluir(request, id):
     produto = Produto.objects.get(id=id)
 
     produto.delete()
 
-    return redirect('cadastrar')
+    return redirect("cadastrar")
